2c65e18c0994de3d629bc24514a3957cd357c1dc
VitalyKapustin-git <workgroupkap@gmail.com>
2021-01-17 23:54:13 +0300

    First commit with changes in koans

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -32,7 +32,7 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
@@ -41,7 +41,22 @@ describe("About Applying What We Have Learnt", function() {
 
       /* solve using filter() & all() / any() */
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      const productsFiltredByNuts = products.filter(
+        (el) => {
+          if (!el.containsNuts) return true;
+        }
+      )
+      
+      productsFiltredByNuts.some(
+        (el) => {
+          if (!el.ingredients.includes("mushrooms")) {
+            productsICanEat.push(el);
+            return true;
+          };
+        }
+      )
+
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
@@ -55,14 +70,25 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
 
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    var sum = 0;    /* try chaining range() and reduce() */
 
-    expect(233168).toBe(FILL_ME_IN);
+    let rangeNums = _.range(1000);
+    sum = rangeNums.reduce(
+      (prev, curr) => {
+        if (curr % 3 === 0 || curr % 5 === 0) {
+          return prev + curr;
+        } else {
+          return prev;
+        }
+      }
+    );
+
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
